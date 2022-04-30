ca748b5b0b5fc09b27d0e5e03c0a721b18846dc1
Peter Sobolewski <pete.sd@gmail.com>
2022-04-30 23:20:02 +0200

    Start on tests: the most basic tests

--- src/napari_sklearn_decomposition/_tests/test_sample_data.py
+++ src/napari_sklearn_decomposition/_tests/test_sample_data.py
@@ -1,7 +1,6 @@
-# from napari_sklearn_decomposition import make_sample_data
+from napari_sklearn_decomposition import faces_sample
 
-# add your tests here...
-
-
-def test_something():
-    pass
+def test_sample_data():
+    # Run faces_sample() and compare the output to the expected output.
+    faces = faces_sample()
+    assert faces[0][0].shape == (400, 64, 64)
--- src/napari_sklearn_decomposition/_tests/test_widget.py
+++ src/napari_sklearn_decomposition/_tests/test_widget.py
@@ -1,33 +1,45 @@
-from napari_sklearn_decomposition import ExampleQWidget, example_magic_widget
-import numpy as np
+from napari_sklearn_decomposition import decomposition
+from typing import Callable
 
-# make_napari_viewer is a pytest fixture that returns a napari viewer object
-# capsys is a pytest fixture that captures stdout and stderr output streams
-def test_example_q_widget(make_napari_viewer, capsys):
-    # make viewer and add an image layer using our fixture
+import napari
+
+
+def test_widget_added(make_napari_viewer: Callable[..., napari.Viewer]) -> None:
+    # Make a viewer
     viewer = make_napari_viewer()
-    viewer.add_image(np.random.random((100, 100)))
-
-    # create our widget, passing in the viewer
-    my_widget = ExampleQWidget(viewer)
-
-    # call our widget method
-    my_widget._on_click()
-
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == "napari has 1 layers\n"
     
-def test_example_magic_widget(make_napari_viewer, capsys):
-    viewer = make_napari_viewer()
-    layer = viewer.add_image(np.random.random((100, 100)))
+    decomp = decomposition()
+    viewer.window.add_dock_widget(decomp)
+    # Check widget was added
+    assert len(viewer.window._dock_widgets) == 1
 
-    # this time, our widget will be a MagicFactory or FunctionGui instance
-    my_widget = example_magic_widget()
+# # make_napari_viewer is a pytest fixture that returns a napari viewer object
+# # capsys is a pytest fixture that captures stdout and stderr output streams
+# def test_example_q_widget(make_napari_viewer, capsys):
+#     # make viewer and add an image layer using our fixture
+#     viewer = make_napari_viewer()
+#     viewer.add_image(np.random.random((100, 100)))
 
-    # if we "call" this object, it'll execute our function
-    my_widget(viewer.layers[0])
+#     # create our widget, passing in the viewer
+#     my_widget = ExampleQWidget(viewer)
 
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == f"you have selected {layer}\n"
+#     # call our widget method
+#     my_widget._on_click()
+
+#     # read captured output and check that it's as we expected
+#     captured = capsys.readouterr()
+#     assert captured.out == "napari has 1 layers\n"
+    
+# def test_example_magic_widget(make_napari_viewer, capsys):
+#     viewer = make_napari_viewer()
+#     layer = viewer.add_image(np.random.random((100, 100)))
+
+#     # this time, our widget will be a MagicFactory or FunctionGui instance
+#     my_widget = example_magic_widget()
+
+#     # if we "call" this object, it'll execute our function
+#     my_widget(viewer.layers[0])
+
+#     # read captured output and check that it's as we expected
+#     captured = capsys.readouterr()
+#     assert captured.out == f"you have selected {layer}\n"
